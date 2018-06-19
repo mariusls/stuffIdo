64cf302e2ebf85f99d09005b17c56cd21d5e53d4
Marius Solvang <mls@itumx.no>
2018-06-19 14:36:52 +0200

    article

--- src/js/absolute-wrapper-heights.js
+++ src/js/absolute-wrapper-heights.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     if ($(window).width() > 767) {
         var finalMargin = ($('.front-textarea').height() / 2 ) + 100;
 
         $('.banner-text-front').css("margin-bottom", finalMargin);
     }
-});
+}); */
