3ea37905cb06d2935a40d4b0f82ba81b0bba9a61
Marius Solvang <mls@itumx.no>
2018-06-20 13:45:09 +0200

    articlepage +++

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('sass', function(){
     return gulp.src('src/scss/main.scss')
         .pipe(sass())
+        .on('error', swallowError)
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({
             stream: true
@@ -27,3 +28,9 @@ gulp.task('watch', ['sass', 'browserSync'], function(){
     gulp.watch('src/*.html', browserSync.reload);
     gulp.watch('src/js/**/*.js', browserSync.reload);
 })
+
+function swallowError (error) {
+    console.log(error.toString())
+
+    this.emit('end')
+}
